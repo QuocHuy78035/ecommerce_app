0ff840b7cb8edc322c900315e9304482d2aa5aa0
quochuywork <huypro0320092003@gmail.com>
2024-03-15 13:06:51 +0700

    update code upload_product

--- lib/vendors/views/screens/vendor_upload_screen.dart
+++ lib/vendors/views/screens/vendor_upload_screen.dart
@@ -1,10 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecomerce_app/providers/product_provider.dart';
+import 'package:ecomerce_app/vendors/views/screens/main_vendor_screen.dart';
 import 'package:ecomerce_app/vendors/views/screens/upload_tap_screens/attributes_screen.dart';
 import 'package:ecomerce_app/vendors/views/screens/upload_tap_screens/general_screen.dart';
 import 'package:ecomerce_app/vendors/views/screens/upload_tap_screens/image_screen.dart';
 import 'package:ecomerce_app/vendors/views/screens/upload_tap_screens/shipping_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:provider/provider.dart';
 import 'package:uuid/uuid.dart';
 
@@ -19,7 +21,8 @@ class _VendorUploadScreenState extends State<VendorUploadScreen> {
   @override
   Widget build(BuildContext context) {
     final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
-    final ProductProvider _productProvider = Provider.of<ProductProvider>(context, listen: false);
+    final ProductProvider _productProvider =
+        Provider.of<ProductProvider>(context, listen: false);
     return DefaultTabController(
       length: 4,
       child: Scaffold(
@@ -60,22 +63,35 @@ class _VendorUploadScreenState extends State<VendorUploadScreen> {
           ],
         ),
         bottomSheet: ElevatedButton(
-          onPressed: ()async {
+          onPressed: () async {
+            EasyLoading.show(status: "Uploading...");
             final productId = const Uuid().v4();
             await _fireStore.collection("products").doc(productId).set({
-              'productId' : productId,
-              'productName' : _productProvider.productData['productName'],
-              "productPrice" : _productProvider.productData['productPrice'],
-              "quantity" : _productProvider.productData['quantity'],
-              "category" : _productProvider.productData['category'],
-              "description" : _productProvider.productData['description'],
-              "imageUrl" : _productProvider.productData['imageUrlList'],
-              "scheduleDate" : _productProvider.productData['scheduleDate'],
-              "chargeShipping" : _productProvider.productData['chargeShipping'],
-              "shippingCharge" : _productProvider.productData['shippingCharge'],
-              "brandName" : _productProvider.productData['brandName'],
-              "sizeList" :_productProvider.productData['sizeList'],
-            });
+              'productId': productId,
+              'productName': _productProvider.productData['productName'],
+              "productPrice": _productProvider.productData['productPrice'],
+              "quantity": _productProvider.productData['quantity'],
+              "category": _productProvider.productData['category'],
+              "description": _productProvider.productData['description'],
+              "imageUrl": _productProvider.productData['imageUrlList'],
+              "scheduleDate": _productProvider.productData['scheduleDate'],
+              "chargeShipping": _productProvider.productData['chargeShipping'],
+              "shippingCharge": _productProvider.productData['shippingCharge'],
+              "brandName": _productProvider.productData['brandName'],
+              "sizeList": _productProvider.productData['listSize'],
+            }).whenComplete(
+              () {
+                EasyLoading.dismiss();
+                print("123");
+                _productProvider.clearData();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const MainVendorScreen(),
+                  ),
+                );
+              },
+            );
           },
           child: const Text("Save"),
         ),
