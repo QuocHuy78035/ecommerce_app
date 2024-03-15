96d653ec1a8f564054ecf5eda0824a6c5c293710
quochuywork <huypro0320092003@gmail.com>
2024-03-15 14:41:02 +0700

    cate selected

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ecomerce_app/buyers/views/buyer/main_screen.dart';
 import 'package:ecomerce_app/providers/product_provider.dart';
 import 'package:ecomerce_app/vendors/views/screens/main_vendor_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -38,7 +39,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MainVendorScreen(),
+      home: const MainCustomerScreen(),
       builder: EasyLoading.init(),
     );
   }
